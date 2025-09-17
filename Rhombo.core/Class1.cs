249a869ae8451d72c8020ff124e79c563d525ffa
Dany-crtl <danielarcila1138700@correo.itm.edu.com>
2025-09-17 02:48:12 -0500

    taller rhombo

---
+++ Rhombo.core/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace Rhombo.core
+{
+    public class GetRhombus
+    {
+        public static string DrawRhombus(int size)
+        {
+            if (size < 1 || size % 2 == 0)
+                throw new ArgumentException("The size must be an integer greater than zero.");
+
+            var rhombus = new System.Text.StringBuilder();
+            int mid = size / 2;
+
+            for (int i = 0; i < size; i++)
+            {
+                int spaces = Math.Abs(mid - i);
+                int stars = size - 2 * spaces;
+                rhombus.Append(' ', spaces);
+
+                for (int j = 0; j < stars; j++)
+                {
+                    if (j == 0 || j == stars - 1)
+                        rhombus.Append('#');
+
+                    else
+                        rhombus.Append(' ');
+                }
+                rhombus.AppendLine();
+            }
+            return rhombus.ToString();
+
+        }
+    }
+}
+
---
+++ rhombo.console/rhombo.console/Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    public class GetRhombus
+    {
+
+        public static string DrawRhombus(int size)
+        {
+            if (size < 1)
+                throw new ArgumentException("The size must be an integer greater than zero.");
+
+            var sb = new System.Text.StringBuilder();
+            int mid = size / 2;
+
+            for (int i = 0; i < size; i++)
+            {
+                int spaces = Math.Abs(mid - i);
+                int stars = size - 2 * spaces - 2;
+
+                sb.Append(' ', spaces);
+
+                if (stars < 0)
+                {
+                    sb.Append('#');
+                }
+                else
+                {
+                    sb.Append('#');
+                    sb.Append(' ', stars);
+                    sb.Append('#');
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el tamaño del rombo:");
+        if (int.TryParse(Console.ReadLine(), out int size))
+        {
+            try
+            {
+                string rhombus = GetRhombus.DrawRhombus(size);
+                Console.WriteLine("\nRombo generado:\n");
+                Console.WriteLine(rhombus);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Eroor: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number greater than zero.");
+        }
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
