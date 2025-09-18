325f9f608bd6a0474ccd922e0bce0460df8f5f18
Dany-crtl <danielarcila1138700@correo.itm.edu.com>
2025-09-17 20:05:59 -0500

    rhombo

--- Rhombo.core/Class1.cs
+++ Rhombo.core/Class1.cs
@@ -2,32 +2,35 @@
 {
     public class GetRhombus
     {
+
         public static string DrawRhombus(int size)
         {
-            if (size < 1 || size % 2 == 0)
+            if (size < 1)
                 throw new ArgumentException("The size must be an integer greater than zero.");
 
-            var rhombus = new System.Text.StringBuilder();
+            var sb = new System.Text.StringBuilder();
             int mid = size / 2;
 
             for (int i = 0; i < size; i++)
             {
                 int spaces = Math.Abs(mid - i);
-                int stars = size - 2 * spaces;
-                rhombus.Append(' ', spaces);
+                int stars = size - 2 * spaces - 2;
 
-                for (int j = 0; j < stars; j++)
+                sb.Append(' ', spaces);
+
+                if (stars < 0)
                 {
-                    if (j == 0 || j == stars - 1)
-                        rhombus.Append('#');
-
-                    else
-                        rhombus.Append(' ');
+                    sb.Append('#');
                 }
-                rhombus.AppendLine();
+                else
+                {
+                    sb.Append('#');
+                    sb.Append(' ', stars);
+                    sb.Append('#');
+                }
+                sb.AppendLine();
             }
-            return rhombus.ToString();
-
+            return sb.ToString();
         }
     }
 }
--- rhombo.console/rhombo.console/Program.cs
+++ rhombo.console/rhombo.console/Program.cs
@@ -1,39 +1,7 @@
-﻿class Program
+﻿using Rhombo.core;
+using System;   
+class Program
 {
-    public class GetRhombus
-    {
-
-        public static string DrawRhombus(int size)
-        {
-            if (size < 1)
-                throw new ArgumentException("The size must be an integer greater than zero.");
-
-            var sb = new System.Text.StringBuilder();
-            int mid = size / 2;
-
-            for (int i = 0; i < size; i++)
-            {
-                int spaces = Math.Abs(mid - i);
-                int stars = size - 2 * spaces - 2;
-
-                sb.Append(' ', spaces);
-
-                if (stars < 0)
-                {
-                    sb.Append('#');
-                }
-                else
-                {
-                    sb.Append('#');
-                    sb.Append(' ', stars);
-                    sb.Append('#');
-                }
-                sb.AppendLine();
-            }
-            return sb.ToString();
-        }
-    }
-
     static void Main()
     {
         Console.WriteLine("Ingrese el tamaño del rombo:");
